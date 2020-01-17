84216159ef29273e03ac15c0299ebf853b1dcc3e
Ruben Rodriguez <ruben.rodriguez@megaterios.co>
2020-01-17 10:20:29 -0500

    feat: Add views index and about

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 router.get('/users/signin', (req, res) => {
-    res.send("Logging in");
+    res.render('users/signin');
 });
 
 router.get('/users/signup', (req, res) => {
-    res.send('User registered');
+    res.render('users/signup');
 });
 
 module.exports = router;
