64b1ec22aab3cfd5b48d6cbc4540fd2a11d277e1
Ruben Rodriguez <ruben.rodriguez@megaterios.co>
2020-01-18 11:28:40 -0500

    feat: Create notes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,4 +1,32 @@
 const router = require('express').Router();
+const Notes = require('../models/Notes');
+
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note');
+});
+
+router.post('/notes/new-note', async (req, res) => {
+    const {title, description} = req.body;
+    const errors = [];
+
+    if(!title){
+        errors.push({text: 'Please you write a title'});
+    }
+    if(!description){
+        errors.push({text: 'Please you write a description'});
+    }
+    if(errors.length > 0){
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    } else{
+        const newNote = new Notes({ title, description });
+        await newNote.save();
+        res.redirect('/notes');
+    }
+});
 
 router.get('/notes', (req, res) => {
     res.send('Notes from database');
